938ddbf425a645b60433ffa29b41fa0d074ae193
Peter Portante <peter.portante@redhat.com>
2020-08-04 20:32:44 -0400

    Fix template error handling; add logging support
    The exceptions were being caught in the wrong order such that
    `ConnectionError` exceptions, which are a sub-class of `TransportError`
    were not being considered.
    
    We also add support for only updating a template when the template's
    mapping version is different.
    
    We add a `logger` argument for `streaming_bulk()` so that we have a
    bit more insight into what is happening while processing actions.
    
    Further, we fix the failing unit tests, add Travis CI integration,
    and ensure the code passes flake8.

--- tests/functests.py
+++ tests/functests.py
@@ -9,11 +9,13 @@ if __name__ == '__main__':
     _op_type = "create"
 
     def _make_source_id(source):
-        return hashlib.md5(json.dumps(source, sort_keys=True).encode('utf-8')).hexdigest()
+        return hashlib.md5(
+            json.dumps(source, sort_keys=True).encode('utf-8')
+        ).hexdigest()
 
     def fake_gen():
         for data in range(0, 10):
-            source = { 'data': data }
+            source = {'data': data}
             action = {
                 _op_type: {
                     "_index":   'test-pyesbulk-streaming_bulk',
@@ -23,7 +25,9 @@ if __name__ == '__main__':
                 }
             yield action, source
 
-    es_client = Elasticsearch([ {"host": sys.argv[1], "port": sys.argv[2] } ], max_retries=0)
-    with open("errors.json", "w") as errorfp):
+    es_client = Elasticsearch(
+        [{"host": sys.argv[1], "port": sys.argv[2]}], max_retries=0
+    )
+    with open("errors.json", "w") as errorfp:
         res = streaming_bulk(es_client, fake_gen(), errorfp)
     print(repr(res))
